09336dfa570731a027213d7935a5260bf21bfb88
command-bat <61804519+command-bat@users.noreply.github.com>
2022-02-10 20:28:42 -0300

    Update escrever.js

--- js/escrever.js
+++ js/escrever.js
@@ -15,3 +15,16 @@ setInterval(function() {
 
 
 }, 7000);
+
+function Mudarestado(el) {
+    var display = document.getElementById(el).style.display;
+    var h2 = document.getElementById("btn");
+
+      if (display == "none") {
+        document.getElementById(el).style.display = 'block';
+        h2.innerHTML = "Esconder";
+      } else {
+        document.getElementById(el).style.display = 'none';
+        h2.innerHTML = "Mostrar";
+      }
+ }
