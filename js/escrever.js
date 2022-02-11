140f8fd3d9812c9d7cae0dae7ec43d1b7e16f78a
command-bat <61804519+command-bat@users.noreply.github.com>
2022-02-11 16:40:08 -0300

    Update escrever.js

--- js/escrever.js
+++ js/escrever.js
@@ -1,10 +1,17 @@
-function Mudarestado(el, btn) {
-    
-    var ele = document.getElementById(el);
-    var display = ele.style.display;
-    
-    ele.style.display = display == 'none' ? 'block' : 'none';
-    btn.innerHTML = display == 'none' ? 'Esconder' : 'Mostrar';
+function typeWriter(elemento) {
+    const textoArray = elemento.innerHTML.split('');
+    elemento.innerHTML = '';
+    for(let i = 0; i < textoArray.length; i++) {
+        setTimeout(() => elemento.innerHTML += textoArray[i], 150 * i);
+     }
+     elemento.innerHTML.replace("aaaaaaa");
+    }
 
-};
-
+    const titulo = document.querySelector('.titulo');
+    
+    setInterval(function(){
+        typeWriter(titulo);
+      
+        
+       
+      }, 7000);
