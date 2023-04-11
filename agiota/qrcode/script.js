05d3e6af8cc9733e6c4f0341e5dff42ac40dcca6
command-bat <61804519+command-bat@users.noreply.github.com>
2023-04-11 19:28:12 -0300

    Update script.js

--- agiota/qrcode/script.js
+++ agiota/qrcode/script.js
@@ -1,12 +1,8 @@
-// Seleciona o elemento "canvas" pelo ID
-const canvas = document.getElementById("qrcode");
-
-// Cria o QR code com a biblioteca "qrcode.js"
-const qrcode = new QRCode(canvas, {
-  text: "https://www.exemplo.com", // Insira o link ou texto que deseja gerar o QR code
-  width: 256,
-  height: 256,
-  colorDark : "#000000",
-  colorLight : "#ffffff",
-  correctLevel : QRCode.CorrectLevel.H
+var qrcode = new QRCode("test", {
+    text: "http://jindo.dev.naver.com/collie",
+    width: 128,
+    height: 128,
+    colorDark : "#000000",
+    colorLight : "#ffffff",
+    correctLevel : QRCode.CorrectLevel.H
 });
