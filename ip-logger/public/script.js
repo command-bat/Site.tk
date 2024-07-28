c5bf1007278ea0ceb6e41db71c653abb57975a0e
command-bat <61804519+command-bat@users.noreply.github.com>
2024-07-28 01:05:43 -0300

    a

--- ip-logger/public/script.js
+++ ip-logger/public/script.js
@@ -1,4 +1,4 @@
-const ws = new WebSocket('wss://ip-logger-rho.vercel.app/');
+const ws = new WebSocket('wss://site-tk.vercel.app');
 
 ws.onmessage = (event) => {
     const ipList = JSON.parse(event.data);
